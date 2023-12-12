325157c1506281c911c32d6ac917b84f6398353d
SegundoPaladines <segundopaladines@gmail.com>
2023-12-12 13:04:13 -0500

    Todas las rutas de mascotas están listas

--- controladores/mascotaControlador.js
+++ controladores/mascotaControlador.js
@@ -60,4 +60,59 @@ const crearMascota = (req, res) => {
     return;
 }
 
-export { crearMascota, listarMascotas, buscarMascota };
+//eliminar una mascota por id
+const eliminarMascota = (req , res) => {
+    const id = parseInt(req.params.id);
+    
+    if(id == null){
+        res.status(400).json({mensaje: "El id no puede estar vacio"});
+        return;
+    }
+
+    mascotas.destroy({
+        where:{pk: id} //recordemos que en la base de datos, el campo de la llave primaria es pk, no id
+    }).then((r) => {
+        res.status(200).json({mensaje: "Mascota eliminada exitosamente"});
+    }).catch((e) => {
+        res.status(500).json({mensaje: "No se ha podido eliminar el registro"});
+    });
+
+    return;
+}
+
+//actualizar una mascota por id
+const actualizarMascota = (req , res) => {
+    const id = parseInt(req.params.id);
+    
+    if(id == null){
+        res.status(400).json({mensaje: "El id no puede estar vacio"});
+        return;
+    }
+
+    if(!req.body.nombre && !req.body.edad && !req.body.tipo_mascota && !req.body.estado){
+        res.status(400).json({mensaje: "No se ha encontrado ningun dato para actualizar"});
+        return;
+    }
+
+    const nombre = req.body.nombre;
+    const edad = req.body.edad;
+    const tipo_mascota = req.body.tipo_mascota;
+    const estado = req.body.estado;
+
+    mascotas.update({
+        nombre: nombre,
+        edad:edad,
+        tipo_mascota:tipo_mascota,
+        estado:estado
+    },{
+        where:{pk: id} //recordemos que en la base de datos, el campo de la llave primaria es pk, no id
+    }).then((r) => {
+        res.status(200).json({mensaje: "Mascota actualizada exitosamente"});
+    }).catch((e) => {
+        res.status(500).json({mensaje: "No se ha podido actualizar el registro"});
+    });
+
+    return;
+}
+
+export { crearMascota, listarMascotas, buscarMascota, eliminarMascota, actualizarMascota };
--- rutas/mascotasRouter.js
+++ rutas/mascotasRouter.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { buscarMascota, crearMascota, listarMascotas } from "../controladores/mascotaControlador.js";
+import { buscarMascota, crearMascota, listarMascotas, eliminarMascota, actualizarMascota } from "../controladores/mascotaControlador.js";
 
 //crear la instancia de tipo router
 const mascostasRouter = express.Router();
@@ -17,6 +17,16 @@ mascostasRouter.get('/buscar/:id',(req, res) => {
 //DE TIPO POST
 mascostasRouter.post('/crear', (req, res)=>{
     crearMascota(req, res);
+});
+
+//Tipo DELETE
+mascostasRouter.delete('/eliminar/:id', (req, res) => {
+    eliminarMascota(req, res);
+});
+
+//Tipo PUT
+mascostasRouter.put('/actualizar/:id', (req, res) => {
+    actualizarMascota(req, res);
 });
 
 export { mascostasRouter };
